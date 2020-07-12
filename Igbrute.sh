42b397d7969f2234a858fc2924209bb9c002b813
SRIVASTAV JI <68119327+3rr0r2208@users.noreply.github.com>
2020-07-12 16:43:02 +0000

    Add files via upload

---
+++ Igbrute.sh
@@ -0,0 +1,105 @@
+#usr/bin/bash
+clear
+bl='\e[30;1m' #black
+re='\e[31;1m' #red
+gr='\e[32;1m' #green
+ye='\e[33;1m' #yellow
+bu='\e[34;1m' #blue
+pi='\e[35;1m' #pink
+cy='\e[36;1m' #cyan
+wh='\e[37;1m' #white
+
+echo
+echo
+
+toilet -f slant -F metal Insta Brute
+echo
+echo $re"                            - A Multipurpose Tool For Instagram"
+echo
+echo
+echo
+echo $bu"  --                                              --"
+echo $bu" |"                    $bu"                                                 |" 
+echo
+echo
+echo              $wh"            Author     :"$cy"     SRIVASTAV JI   "                
+echo                                                    
+echo              $wh"            Instagram  :"$cy"     srivastav_ji_23 "
+echo           
+echo
+echo $bu" |"                    $bu"                                                 |" 
+echo $bu"  --                                              --"
+echo
+echo
+echo
+echo
+echo $pi"  --                                                      --"
+echo $pi" |                                                   "$pi"       |"
+echo        $gr"      1"$gr"       =>"$cy"                BRUTEFORCE INSTA 1              " 
+echo $pi" |                                                   "$pi"       |"
+echo        $gr"      2"$gr"       =>"$cy"                BRUTEFORCE INSTA 2(root)        " 
+echo $pi" |                                                   "$pi"       |"
+echo        $gr"      3"$gr"       =>"$cy"                INSTA (Multi)                   "
+echo $pi" |                                                   "$pi"       |"
+echo        $gr"      4"$gr"       =>"$cy"                PHISHING   INSTA 1(Multi)       "
+echo $pi" |                                                   "$pi"       |"
+echo        $gr"      5"$gr"       =>"$cy"                PHISHING   INSTA 2(Multi)       "
+echo $pi" |                                                   "$pi"       |"
+echo $pi"  --                                                      --"
+echo
+echo
+echo $re"┌─"$re"["$gr"root@kali"$re"]"$re"-"$re"["$gr""By Srivastav Ji""$re"]"
+echo $re"|"
+read -p"└──╼ #" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|
+sleep 1
+git clone https://github.com/fuck3erboy/instahack
+cd instahack
+python3 hackinsta.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|
+sleep 1
+git clone https://github.com/Ha3MrX/InstaBrute
+cd InstaBrute
+bash insta.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|
+sleep 1
+git clone https://github.com/Swetabhsuman8/instagram-hacking-tool
+cd  instagram-hacking-tool
+bash instagram-hacking-tool.sh
+fi
+
+if [ $pil = 4]
+then
+clear
+figlet -f slant "W A I T"|
+sleep 1
+git clone https://github.com/S1lkys/shellphishSS
+cd  shellphishSS 
+bash shellphish.sh  
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|
+sleep 1
+git clone https://github.com/Clow3nSec/clow3nphish
+cd clow3nphish
+python3 clow3nphish.py
+fi
+
+
